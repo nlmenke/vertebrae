240e1cc901a6d3199929f345cd169c8258a995da
Nick Menke <n.menke@ubreakifix.com>
2018-10-16 04:40:07 +0000

    Apply fixes from StyleCI

--- app/Providers/RouterServiceProvider.php
+++ app/Providers/RouterServiceProvider.php
@@ -3,7 +3,7 @@
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Route;
 
-class RouteServiceProvider extends ServiceProvider
+class RouterServiceProvider extends ServiceProvider
 {
     /**
      * This namespace is applied to your controller routes.
