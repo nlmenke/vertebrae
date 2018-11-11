7c8f25fded67b908e57166b572711c80f5956cbf
Nick Menke <nick@nlmenke.net>
2018-11-11 03:42:14 -0500

    currency testing

--- database/factories/CurrencyFactory.php
+++ database/factories/CurrencyFactory.php
@@ -7,10 +7,11 @@ use Illuminate\Database\Eloquent\Factory;
 /** @var Factory $factory */
 $factory->define(Currency::class, function (Faker $faker) {
     return [
-        'iso_alpha' => $faker->currencyCode,
-        'iso_number' => str_pad($faker->randomNumber(3), 3, 0, STR_PAD_LEFT),
+        'iso_alpha' => $faker->unique()->currencyCode,
+        'iso_numeric' => str_pad($faker->unique()->randomNumber(2), 3, 0, STR_PAD_LEFT),
         'name' => $faker->country . ' Dollar',
         'symbol' => $faker->randomAscii,
-        'exchange_rate' => floatVal($faker->randomNumber()),
+        'decimal_precision' => $faker->randomNumber(1),
+        'exchange_rate' => $faker->randomFloat(6, 0, 999999),
     ];
 });
--- tests/Feature/CurrencyTest.php
+++ tests/Feature/CurrencyTest.php
@@ -1,18 +1,170 @@
 <?php namespace Tests\Feature;
 
+use App\Entities\Currency\Currency;
+use Illuminate\Http\Response;
 use Tests\TestCase;
 
 class CurrencyTest extends TestCase
 {
     /**
-     * A basic test example.
+     * List can be viewed via API.
      *
+     * @test
      * @return void
      */
-    public function testBasicTest(): void
+    public function canViewCurrencies(): void
     {
-        $response = $this->get('api/currencies');
+        $currencies = factory(Currency::class, 10)->create();
 
-        $response->assertStatus(200);
+        $response = $this->get(route('currencies.index'));
+        $response->assertStatus(Response::HTTP_OK)
+            ->assertJson([
+                'data' => $currencies->toArray()
+            ]);
+    }
+
+    /**
+     * Show can be viewed via API.
+     *
+     * @test
+     * @return void
+     */
+    public function canViewCurrency(): void
+    {
+        $currency = factory(Currency::class)->create();
+
+        $response = $this->get(route('currencies.show', $currency->getId()));
+        $response->assertStatus(Response::HTTP_OK)
+            ->assertJson([
+                'data' => $currency->toArray()
+            ]);
+    }
+
+    /**
+     * Currency can be created via API.
+     *
+     * @test
+     * @return void
+     */
+    public function canCreateCurrency(): void
+    {
+        $data = factory(Currency::class)->make()->toArray();
+
+        $response = $this->postJson(route('currencies.store'), $data);
+        $response->assertJsonMissingValidationErrors([
+                'iso_alpha',
+                'iso_numeric',
+                'name',
+                'symbol',
+                'decimal_precision',
+                'exchange_rate',
+            ])
+            ->assertStatus(Response::HTTP_CREATED)
+            ->assertJson([
+                'data' => $data
+            ]);
+
+        // verify newly created currency exists
+        $response = $this->get($response->headers->get('Location'));
+        $response->assertStatus(Response::HTTP_OK)
+            ->assertJson([
+                'data' => $data
+            ]);
+    }
+
+    /**
+     * Validate input for create API.
+     *
+     * @test
+     * @return void
+     */
+    public function creatingCurrencyRequiresName(): void
+    {
+        $data = factory(Currency::class)->make(['name' => ''])->toArray();
+
+        $response = $this->postJson(route('currencies.store'), $data);
+        $response->assertJsonValidationErrors([
+                'name',
+            ])
+            ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertJson([
+                'errors' => [
+                    'name' => ['The name field is required.']
+                ]
+            ]);
+    }
+
+    /**
+     * Currency can be updated via API.
+     *
+     * @test
+     * @return void
+     */
+    public function canUpdateCurrency(): void
+    {
+        $currency = factory(Currency::class)->create();
+        $data = factory(Currency::class)->make()->toArray();
+
+        $response = $this->putJson(route('currencies.update', $currency->getId()), $data);
+        $response->assertJsonMissingValidationErrors([
+                'iso_alpha',
+                'iso_numeric',
+                'name',
+                'symbol',
+                'decimal_precision',
+                'exchange_rate',
+            ])
+            ->assertStatus(Response::HTTP_OK)
+            ->assertJson([
+                'data' => $data
+            ]);
+
+        // verify currency was updated
+        $response = $this->get($response->headers->get('Location'));
+        $response->assertStatus(Response::HTTP_OK)
+            ->assertJson([
+                'data' => $data
+            ]);
+    }
+
+    /**
+     * Validate input for update API.
+     *
+     * @test
+     * @return void
+     */
+    public function updatingCurrencyRequiresName(): void
+    {
+        $currency = factory(Currency::class)->create();
+        $data = factory(Currency::class)->make(['name' => ''])->toArray();
+
+        $response = $this->putJson(route('currencies.update', $currency->getId()), $data);
+        $response->assertJsonValidationErrors([
+                'name',
+            ])
+            ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertJson([
+                'errors' => [
+                    'name' => ['The name field is required.']
+                ]
+            ]);
+    }
+
+    /**
+     * Currency can be deleted via API.
+     *
+     * @test
+     * @return void
+     */
+    public function canDeleteCurrency(): void
+    {
+        $currency = factory(Currency::class)->create();
+
+        $response = $this->deleteJson(route('currencies.destroy', $currency->getId()));
+        $response->assertStatus(Response::HTTP_NO_CONTENT);
+
+        // verify currency was deleted
+        $response = $this->get(route('currencies.show', $currency->getId()));
+        $response->assertStatus(Response::HTTP_NOT_FOUND);
     }
 }
